f3ef4cefe1fbcd48037dd3fe02b5066e6eadba88
gokulnathanT <mailtogokul1727@gmail.com>
2024-08-21 01:15:10 +0530

    changes made

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,23 +1,25 @@
 public class RewardValue {
     private double cashValue;
-    private double milesValue;
     private static double rate=0.0035;
 
 
     public RewardValue(double cashValue) {
         this.cashValue=cashValue;
-        this.milesValue=cashValue/rate;
-    }
 
-    public RewardValue(double milesValue,boolean miles) {
-        if(miles){
-            this.cashValue =milesValue*rate;
-            this.milesValue = milesValue;
-        }
+    }
+    public RewardValue(int milesValue) {
+        this.cashValue=converttoCash(milesValue);
+
+    }
+    private static int converttoMiles(double cashValue){
+        return (int) (cashValue/rate);
+    }
+    private static double converttoCash(int milesValue) {
+        return milesValue * rate;
     }
 
     public double getMilesValue() {
-        return milesValue;
+        return converttoMiles(cashValue);
     }
     public double getCashValue(){
         return cashValue;
